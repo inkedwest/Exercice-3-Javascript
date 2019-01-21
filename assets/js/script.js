3e016e21e721b8e01150acde457b03c39831c0b6
Glatigny <f.west@live.fr>
2019-01-21 10:40:00 +0100

    exerccie 3

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var lastName = 'Doe';
+var firstName = 'John';
+var city = 'New York';
+alert("Nom : " + lastName + "\nPrénom : " +firstName +"\nVille : " +city)
+"Hello there " + name + ". Welcome."
